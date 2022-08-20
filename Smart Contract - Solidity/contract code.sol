08653dcc385bd629d7d8b372f8a91649a2818afe
cameron7702 <camsmith0707@gmail.com>
2022-08-20 09:52:39 -0400

    CADs

---
+++ Smart Contract - Solidity/contract code.sol
@@ -0,0 +1,52 @@
+pragma solidity >= 0.5.10;
+string[] Shashank = [20, "gender = male ", "single","kids=none", "student", "income = >50000", "Insurance = none", "citizen = Canadian"];
+
+
+string language = "English";
+string profiency = "High";
+
+contract financeForShashank {
+    vint256 Shashank;
+
+    function get() public view returns (unit) {
+        return Shashank;
+    }
+
+price_per_month = 100;
+
+// funding for maretial status deducton based off of funding from government grants, ngos etc.. (would have partners when doing this)
+
+if Integer.parseInt(Shashank[0]) < 21{ //age 
+    price_per_month= price_per_ month*0.98; //2% discount 
+}
+
+if Shashank [1] != "male" { //gender 
+    price_per_month= price_per_month*0.98; //2% discount 
+}
+
+if Shashank [2] == "single" { //marital status
+    price_per_month= price_per_month*0.98; //2% discount 
+}
+
+if Shashank [3] == "kids=true" { //childerne 
+    price_per_month= price_per_month*0.85; //15% discount 
+}
+
+if Shashank [4] == "student" { //student 
+    price_per_month= price_per_month*0.98; //2% discount 
+}
+
+if Shashank [5] == true { //income
+    price_per_month= price_per_month*0.50; //50% discount 
+}
+
+if Shashank [6] == false { //insurance 
+    price_per_month= price_per_month*0.95; //25% discount 
+}
+
+if Shashank [7] == "Canadian" { //citizenship 
+    price_per_month= price_per_month*1; //0% discount 
+}
+
+
+}
